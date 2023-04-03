7f9d2b29975ed845e313ec4cefe825a536d24fc5
juusoforsm <juuso.forsman@gmail.com>
2023-04-03 09:24:02 +0300

    created html files

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/guestbook', function (req, res) {
+    res.sendFile(__dirname + '/public/guestbook.html');
+});
+
+app.get('/newmessage', function (req, res) {
+    res.sendFile(__dirname + '/public/newmessage.html');
+});
+
+app.get('/ajaxmessage', function (req, res) {
+    res.sendFile(__dirname + '/public/ajaxmessage.html');
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, function () {
