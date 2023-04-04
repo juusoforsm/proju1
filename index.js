c53a54317c6d7f59b239d7858508ef310addf568
juusoforsm <juuso.forsman@gmail.com>
2023-04-04 21:48:50 +0300

    Finished the project

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public/img/'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
@@ -18,6 +21,41 @@ app.get('/newmessage', function (req, res) {
 app.get('/ajaxmessage', function (req, res) {
     res.sendFile(__dirname + '/public/ajaxmessage.html');
 });
+
+app.get('/json', function (req, res) {
+    if (!fs.existsSync('public/guestbook.json')) {
+        fs.writeFileSync('./public/guestbook.json', '[]');
+    }
+
+    res.sendFile(__dirname + "/public/guestbook.json");
+})
+
+
+app.get('*', function (req, res) {
+    res.setHeader('content-type', 'text/plain');
+    res.status(404).send('404 not found');
+});
+
+app.post('/addmessage', function (req, res) {
+    if (!fs.existsSync('./public/guestbook.json')) {
+        fs.writeFileSync('./public/guestbook.json', '[]');
+    }
+    let data = require('./public/guestbook.json');
+
+    data.push({
+        "username": req.body.username,
+        "country": req.body.country,
+        "message": req.body.message,
+        "date": Date()
+    })
+    let text = JSON.stringify(data, false, 4)
+    fs.writeFile('./public/guestbook.json', text, (err) => {
+        if (err) throw err;
+        console.log('You actually did it!');
+    })
+    res.send(data);
+
+})
 
 const port = process.env.PORT || 8080;
 app.listen(port, function () {
